826edbea743373d09c1f70fd5eaa66153f68ecd3
Pinefoot <jameslcarpino@gmail.com>
2020-10-26 16:08:36 -0700

    added some more components, scaffolding continuing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Landing from "./components/Landing";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Landing></Landing>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Landing.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Landing() {
+  return (
+    <div>
+      <h1>Welcome. Thanks for swingin' through</h1>
+    </div>
+  );
+}
