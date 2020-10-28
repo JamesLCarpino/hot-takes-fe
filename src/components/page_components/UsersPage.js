03005bea3401b03d929d35695c45b34f41800c91
Pinefoot <jameslcarpino@gmail.com>
2020-10-28 15:17:02 -0700

    sign out working

--- src/components/page_components/UsersPage.js
+++ src/components/page_components/UsersPage.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import { useHistory } from "react-router-dom";
 export default function UsersPage() {
+  const history = useHistory();
+  const logOut = (e) => {
+    localStorage.clear("token");
+    history.push("/");
+  };
   return (
     <div>
       <h1>User Page</h1>
+      <button onClick={logOut}>sign out</button>
     </div>
   );
 }
