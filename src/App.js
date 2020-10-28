3e128b0fc5364da163826fb5c98d429102a535cb
Pinefoot <jameslcarpino@gmail.com>
2020-10-28 14:19:15 -0700

    routes set

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import { Route, Link, NavLink, Switch } from "react-router-dom";
 import PrivateRoute from "./utils/PrivateRoute";
 import Login from "./components/auth_components/Login";
 import Register from "./components/auth_components/Register";
-
+import UsersPage from "./components/page_components/UsersPage";
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route exact path="/register" component={Register} />
-
         <Route exact path="/login" component={Login} />
+        <PrivateRoute exact path="/my-home" component={UsersPage} />
+        {/* <PrivateRoute exact path="/hot-takes" component={HotTakesPage}/> */}
       </Switch>
     </div>
   );
